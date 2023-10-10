9f80b03397fdc53b6d6248c8f9a6d807d4e7d70f
Midhun A Darvin <midhunadarvin@gmail.com>
2023-10-10 21:28:53 +0530

    feat: add HTTP Protocol handler helper methods

--- src/BaseProtocolHandler.h
+++ src/BaseProtocolHandler.h
@@ -6,6 +6,7 @@
 #include "CommonTypes.h"
 #include "Utils.h"
 #include <map>
+#include "http_message.h"
 
 class BaseProtocolHandler : public CProtocolHandler
 {
@@ -15,18 +16,19 @@ public:
      * @param buffer - the buffer that we receive from upstream ( source dbs )
      * @param length - length of the buffer
      */
-    std::string HandleData(void *buffer, int buffer_length, EXECUTION_CONTEXT *exec_context)
+    std::string HandleData(void *request_string, int buffer_length, EXECUTION_CONTEXT *exec_context)
     {
-        // Respond with a simple "Hello, World!" message
-        // std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello, World!";
-
         string response = "";
         response += "HTTP/1.1 200 OK\r\n";
-        response+="Content-Length: 12\r\n";
+        response += "Content-Type: text/plain\r\n";
+        response += "Content-Length: 12\r\n";
         response += "\r\n";
         response += "Hello World!";
 
-        return response;
-    }
+        size_t response_length = strlen(response.c_str());
+        char *result = (char *)malloc(response_length);
+        memcpy(result, response.c_str(), response_length);
 
+        return result;
+    }
 };
--- src/CHttpProtocolHandler.h
+++ src/CHttpProtocolHandler.h
@@ -2,12 +2,32 @@
 
 #include "BaseProtocolHandler.h"
 
-class CHttpProtocolHandler : public BaseProtocolHandler {
+// A request handler should expect a request as argument and returns a response
+using HttpRequestHandler_t = std::function<simple_http_server::HttpResponse(const simple_http_server::HttpRequest &)>;
+
+class CHttpProtocolHandler : public BaseProtocolHandler
+{
+private:
+    std::map<simple_http_server::Uri, std::map<simple_http_server::HttpMethod, HttpRequestHandler_t>> request_handlers_;
+
 public:
     CHttpProtocolHandler() {}
 
-    //virtual std::string  HandleData(void *Buffer, int len, CLIENT_DATA &clientData);
+    void RegisterHttpRequestHandler(const std::string &path, simple_http_server::HttpMethod method,
+                                    const HttpRequestHandler_t callback)
+    {
+        simple_http_server::Uri uri(path);
+        request_handlers_[uri].insert(std::make_pair(method, std::move(callback)));
+    }
 
-     ~CHttpProtocolHandler() {}
+    void RegisterHttpRequestHandler(const simple_http_server::Uri &uri, simple_http_server::HttpMethod method,
+                                    const HttpRequestHandler_t callback)
+    {
+        request_handlers_[uri].insert(std::make_pair(method, std::move(callback)));
+    }
 
+    virtual std::string HandleData(void *request_string, int buffer_length, EXECUTION_CONTEXT *exec_context);
+    simple_http_server::HttpResponse HandleHttpRequest(const simple_http_server::HttpRequest &request);
+
+    ~CHttpProtocolHandler() {}
 };
