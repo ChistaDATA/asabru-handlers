de169ec8840e4f4fbdddd5827b025da141906cc3
josephabraham <joseph.abraham@chistadata.com>
2023-12-28 19:42:35 +0530

    feat : Proxy Change Handler for Zookeeper events

---
+++ src/request-handlers/UpdateEndPointServiceCommand.cpp
@@ -0,0 +1,32 @@
+#include "UpdateEndPointServiceCommand.h"
+
+
+extern "C" UpdateEndPointServiceCommand *createUpdateEndPointServiceCommand()
+{
+    return new UpdateEndPointServiceCommand;
+}
+
+extern "C" void  destroyUpdateEndPointServiceCommand(UpdateEndPointServiceCommand *c)
+{
+    delete c;
+}
+
+bool  UpdateEndPointServiceCommand::Execute(ComputationContext *context)
+{
+    auto *request = std::any_cast<const simple_http_server::HttpRequest *>(context->Get("request"));
+    std::any functionAny = context->Get("update_endpoint_service");
+
+    // Cast and call the function using std::any
+    if (functionAny.type() == typeid(std::string (*)(std::string))) {
+        auto updateEndPointService = std::any_cast<std::string (*)(std::string)>(functionAny);
+        std::string response_content = updateEndPointService(request->content());
+        auto *response = new simple_http_server::HttpResponse(simple_http_server::HttpStatusCode::Ok);
+        response->SetHeader("Content-Type", "text/plain");
+        response->SetContent(response_content);
+        context->Put("response", response);
+        return true;
+    } else {
+        std::cout << "Function pointer type mismatch." << std::endl;
+        return false;
+    }
+}
