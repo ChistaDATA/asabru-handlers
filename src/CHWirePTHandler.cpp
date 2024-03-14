edbbb1b098bf740d8eb3e89bb854bc0d82e51ea3
Midhun A Darvin <midhunadarvin@gmail.com>
2024-03-15 01:02:57 +0530

    feat: add clickhouse wire protocol parser
    - implement wire protocol parsing logic

--- src/CHWirePTHandler.cpp
+++ src/CHWirePTHandler.cpp
@@ -2,6 +2,49 @@
 #include "CProxyHandler.h"
 #include "CHWirePTHandler.h"
 
+/**
+ * Function that handles upstream data
+ * @param buffer - the buffer that we receive from upstream ( source dbs )
+ * @param length - length of the buffer
+ */
+std::string CHWirePTHandler::HandleUpstreamData(std::string buffer, ssize_t buffer_length, EXECUTION_CONTEXT *exec_context)
+{
+    LOG_INFO("Received a Client packet..................... ");
+    LOG_INFO("Length of Packet is " + std::to_string(buffer_length) );
+    LOG_INFO("Packet Type = " + std::to_string((int) *((unsigned char *)buffer.c_str())) );
+
+    QueryType queryType;
+    uint64_t packet_type = 0;
+    char * temp = &buffer[0];
+
+    readVarUInt(packet_type, temp);
+    if (packet_type == 0)
+    {
+        parser->parseClientHello(temp, buffer_length);
+    }
+    else if (packet_type == 1)
+    {
+        parser->parseClientQuery(temp, buffer_length, exec_context);
+    }
+
+    parser->LogResponse((char *) buffer.c_str(), buffer_length);
+    return buffer;
+}
+
+/**
+ * Function that handles downstream data
+ * @param buffer - the buffer / response that we receive from downstream ( target dbs )
+ * @param length - length of the buffer
+ */
+std::string CHWirePTHandler::HandleDownStreamData(std::string buffer, ssize_t buffer_length, EXECUTION_CONTEXT *exec_context)
+{
+    LOG_INFO("Received a Server packet..................... ");
+    LOG_INFO("Length of Packet is " + std::to_string(buffer_length) );
+    LOG_INFO("Packet Type = " + std::to_string((int) *((unsigned char *)buffer.c_str())) );
+
+    return buffer;
+}
+
 extern "C" CHWirePTHandler *createCHWirePTHandler()
 {
     return new CHWirePTHandler;
--- src/CHWirePTHandler.h
+++ src/CHWirePTHandler.h
@@ -1,13 +1,28 @@
-#ifndef CH_WIRE_PROTOCOL_HANDLER_H
-#define CH_WIRE_PROTOCOL_HANDLER_H
+#pragma once
 
 #include "CServerSocket.h"
 #include "BaseProxyHandler.h"
+#include "CHWirePTParser.h"
 
 class CHWirePTHandler : public BaseProxyHandler {
-
+private:
+    CHWirePTParser *parser = 0;
 public:
-    CHWirePTHandler() {}
-};
+    CHWirePTHandler() {
+        parser = new CHWirePTParser();
+    }
 
-#endif
+    /**
+     * Function that handles upstream data
+     * @param buffer - the buffer that we receive from upstream ( source dbs )
+     * @param length - length of the buffer
+     */
+    std::string HandleUpstreamData(std::string buffer, ssize_t buffer_length, EXECUTION_CONTEXT *exec_context) override;
+
+    /**
+     * Function that handles downstream data
+     * @param buffer - the buffer / response that we receive from downstream ( target dbs )
+     * @param length - length of the buffer
+     */
+    std::string HandleDownStreamData(std::string buffer, ssize_t buffer_length, EXECUTION_CONTEXT *exec_context) override;
+};
